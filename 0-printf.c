a652682040d6af59955f7faf9b42ba6ec7485ae8
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-04 03:19:05 +0100

    splitted code into 2 files

--- 0-printf.c
+++ 0-printf.c
@@ -48,30 +48,6 @@ int _printf(const char *format, ...)
 }
 
 /**
-  * print_char - writes a char to stdout
-  * @c: the char
-  * Return: number of char printed = 1
-  */
-int print_char(char *c)
-{
-	if (c == NULL)
-		return (0);
-	return (write(1, c, 1));
-}
-
-/**
-  * print_str - writes a string to stdout
-  * @s: the string
-  * Return: the number of chars written
-  */
-int print_str(char *s)
-{
-	if (s == NULL || *s == 0)
-		return (0);
-	return (write(1, s, len(s)));
-}
-
-/**
   * len - finds the length of a string
   * @s: the string
   * Return: length of s
